588f8264e0538c6ccd3028f9413e8444c6b411a2
MaeWolff <maxencewolff.pro@gmail.com>
2025-03-19 00:20:41 +0100

    feat: add color round filters and improve some details

--- src/entrypoints/js/header.js
+++ src/entrypoints/js/header.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.getElementById('mobile-menu-button');
   const mobileMenu = document.getElementById('mobile-menu');
   const closeMenuButton = document.getElementById('close-mobile-menu');
+  const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
 
   function openMenu() {
     document.body.style.overflow = 'hidden';
 
     mobileMenu.classList.remove('-translate-x-full');
     mobileMenu.classList.add('translate-x-0');
+
+    if (mobileMenuOverlay) {
+      mobileMenuOverlay.classList.remove('hidden');
+      mobileMenuOverlay.classList.add('block');
+    }
   }
 
   function closeMenu() {
@@ -15,6 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     mobileMenu.classList.remove('translate-x-0');
     mobileMenu.classList.add('-translate-x-full');
+
+    if (mobileMenuOverlay) {
+      mobileMenuOverlay.classList.remove('block');
+      mobileMenuOverlay.classList.add('hidden');
+    }
   }
 
   menuButton.addEventListener('click', (e) => {
@@ -23,6 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   closeMenuButton.addEventListener('click', closeMenu);
+
+  // Fermer le menu lorsqu'on clique sur l'overlay
+  if (mobileMenuOverlay) {
+    mobileMenuOverlay.addEventListener('click', closeMenu);
+  }
 
   document.addEventListener('click', (e) => {
     if (!mobileMenu.contains(e.target) && e.target !== menuButton) {
